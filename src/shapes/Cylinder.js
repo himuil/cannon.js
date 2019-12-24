7bfdcb2aa6a6b798c5b893518bfa484e25271318
JayceLai <goodlaijiaxin@qq.com>
2019-12-24 14:15:17 +0800

    set cylinder default axis to y

--- src/shapes/Cylinder.js
+++ src/shapes/Cylinder.js
@@ -17,63 +17,40 @@ var ConvexPolyhedron = require('./ConvexPolyhedron');
  */
 function Cylinder( radiusTop, radiusBottom, height , numSegments ) {
     var N = numSegments,
-        verts = [],
-        axes = [],
-        faces = [],
-        bottomface = [],
-        topface = [],
         cos = Math.cos,
         sin = Math.sin;
 
-    // First bottom point
-    verts.push(new Vec3(radiusBottom*cos(0),
-                               radiusBottom*sin(0),
-                               -height*0.5));
-    bottomface.push(0);
+    var halfH = height/2;
+    var vertices = [];
+    var indices = [];
+    var tf = [0];
+    var bf = [1];
+    var axes = [];
+    var theta = Math.PI * 2 / N;
+    for(var i = 0; i < N; i++){
+        vertices.push(new Vec3(radiusTop * Math.cos(theta*i), halfH, radiusTop * Math.sin(theta*i)));
+        vertices.push(new Vec3(radiusTop * Math.cos(theta*i), -halfH, radiusTop * Math.sin(theta*i)));
 
-    // First top point
-    verts.push(new Vec3(radiusTop*cos(0),
-                               radiusTop*sin(0),
-                               height*0.5));
-    topface.push(1);
-
-    for(var i=0; i<N; i++){
-        var theta = 2*Math.PI/N * (i+1);
-        var thetaN = 2*Math.PI/N * (i+0.5);
-        if(i<N-1){
-            // Bottom
-            verts.push(new Vec3(radiusBottom*cos(theta),
-                                       radiusBottom*sin(theta),
-                                       -height*0.5));
-            bottomface.push(2*i+2);
-            // Top
-            verts.push(new Vec3(radiusTop*cos(theta),
-                                       radiusTop*sin(theta),
-                                       height*0.5));
-            topface.push(2*i+3);
-
-            // Face
-            faces.push([2*i+2, 2*i+3, 2*i+1,2*i]);
-        } else {
-            faces.push([0,1, 2*i+1, 2*i]); // Connect
+        if(i < N-1){
+            indices.push([2*i+2, 2*i+3, 2*i+1,2*i]);
+            tf.push(2*i+2);
+            bf.push(2*i+3);
+        }else{
+            indices.push([0,1, 2*i+1, 2*i]);
         }
 
-        // Axis: we can cut off half of them if we have even number of segments
         if(N % 2 === 1 || i < N / 2){
-            axes.push(new Vec3(cos(thetaN), sin(thetaN), 0));
+            axes.push(new Vec3(cos(theta*(i+0.5)), 0, sin(theta*(i+0.5))));
         }
     }
-    faces.push(topface);
-    axes.push(new Vec3(0,0,1));
-
-    // Reorder bottom face
+    indices.push(bf);
     var temp = [];
-    for(var i=0; i<bottomface.length; i++){
-        temp.push(bottomface[bottomface.length - i - 1]);
+    for(var i=0; i<tf.length; i++){
+        temp.push(tf[tf.length - i - 1]);
     }
-    faces.push(temp);
-
-    ConvexPolyhedron.call( this, verts, faces, axes );
+    indices.push(temp);
+    axes.push(new Vec3(0,1,0));
+    ConvexPolyhedron.call( this, vertices, indices, axes);
 }
 
 Cylinder.prototype = new ConvexPolyhedron();
